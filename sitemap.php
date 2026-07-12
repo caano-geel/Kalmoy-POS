3ffb1d820ccea44e544491b3a219b6898b665d2e
Abdullahi Omar <abdullahiomarsalad@gmail.com>
2026-07-12 04:38:03 +0300

    Fix sitemap XML response for Google Search Console

--- sitemap.php
+++ sitemap.php
@@ -1,4 +1,20 @@
 <?php
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/initialize.php';
+require_once __DIR__ . '/classes/DBConnection.php';
+
+$db = new DBConnection();
+$conn = $db->conn;
+
 require_once __DIR__ . '/inc/seo.php';
+
+while (ob_get_level() > 0) {
+    ob_end_clean();
+}
+
+if (!headers_sent()) {
+    header('Content-Type: application/xml; charset=UTF-8');
+    header('X-Robots-Tag: noindex', true);
+}
+
 seo_render_sitemap_xml();
+exit;
